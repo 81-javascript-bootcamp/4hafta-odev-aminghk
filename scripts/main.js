d3f81ad5c67df1f12bbb94a043147aa7573582b2
aminghk <aminghanbari@hotmail.com>
2021-02-20 18:46:32 +0000

    bu hafta bilgisayarsiz oldugum icin odeve tam bakamadim yeni bilgisayarim bugun elime ulasti oldugu kadarini yapmaya calistim. bir sonraki haftaya ozel bir odev verilirse telafi sansim olur.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,8 @@ class MoviesApp {
         const {root, searchInput, searchForm, yearHandler, yearSubmitter} = options;
         this.$tableEl = document.getElementById(root);
         this.$tbodyEl = this.$tableEl.querySelector("tbody");
-
+        this.$yearFilter = document.getElementById("yearFilter");
+        this.$genreFilter = document.getElementById("genreFilter");
         this.$searchInput = document.getElementById(searchInput);
         this.$searchForm   = document.getElementById(searchForm);
         this.yearHandler = yearHandler;
@@ -15,7 +16,32 @@ class MoviesApp {
 
     createMovieEl(movie){
         const {image, title, genre, year,id} = movie;
-        return `<tr data-id="${id}"><td><img src="${image}"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`
+        return `<tr data-id="${id}">
+        <td><img src="${image}"></td>
+        <td>${title}</td>
+        <td>${genre}</td>
+        <td>${year}</td>
+        </tr>`
+    }
+
+    createYearEl(yearData){
+        const {year,occu} = yearData;
+        return `<div class="form-check">
+            <input class="form-check-input" type="radio" name="year" id="year1" value="${year}">
+            <label class="form-check-label" for="year">
+            ${year} (${occu})
+            </label>
+            </div>`
+    }
+
+    createGenreEl(genreData){
+        const {genre,occu} = genreData;
+        return `<div class="form-check">
+        <input class="form-check-input" type="checkbox" value="${genre}" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+            ${genre} (${occu})
+        </label>
+    </div>`
     }
 
     fillTable(){
@@ -26,6 +52,7 @@ class MoviesApp {
            return this.createMovieEl(movie)
         }).join("");
         this.$tbodyEl.innerHTML = moviesArr;
+       
     }
 
     reset(){
@@ -46,6 +73,77 @@ class MoviesApp {
 
         });
     }
+    
+ 
+
+
+    addYearsToFiter(){
+
+        const years = []
+        data.forEach((movie)=>{
+            let year = movie.year
+            years.push(year)
+        })
+        const yearsObject = []
+        years.forEach((item)=>{
+            let itemCount = 1;
+            for (let i = 0; i < years.length; i++) {
+                const element = years[i];
+                if(item === element){
+                    itemCount++;
+                    years.splice(i, 1); 
+                } 
+            }
+            let tempData = {
+                year : item,
+                occu : itemCount
+            }
+            yearsObject.push(tempData);
+        }) 
+        console.log(yearsObject)  
+        const yearArr = yearsObject.map((yearData) => {
+            return this.createYearEl(yearData)
+         }).join("");
+        this.$yearFilter.innerHTML = yearArr;
+       
+    }
+    addGenresToFilter(){
+
+
+        const genres = []
+        data.forEach((movie)=>{
+            let genre = movie.genre
+            genres.push(genre)
+        })
+        
+      
+        
+        const genresObject = []
+        genres.forEach((item)=>{
+            let itemCount = 1;
+
+            for (let i = 0; i < genres.length; i++) {
+                const element = genres[i];
+                if(item === element){
+                    itemCount++;
+                    genres.splice(i, 1); 
+                } 
+            }
+            let tempGenre = {
+                genre : item,
+                occu : itemCount
+            }
+            genresObject.push(tempGenre);
+        })   
+
+        console.log(genresObject)
+        const genreArr = genresObject.map((genreData) => {
+            return this.createGenreEl(genreData)
+         }).join("");
+        this.$genreFilter.innerHTML = genreArr;
+        
+
+    }
 
     handleYearFilter(){
         this.$yearSubmitter.addEventListener("click", () => {
@@ -61,6 +159,8 @@ class MoviesApp {
         this.fillTable();
         this.handleSearch();
         this.handleYearFilter();
+        this.addYearsToFiter();
+        this.addGenresToFilter();
     }
 }
 
